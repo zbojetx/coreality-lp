c13af51d88e4938c77a007c1dbe695c4f2ef30b7
zbojetx <zbojetx@gmail.com>
2021-03-23 11:15:30 +0700

    sticky on dekstop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
-function mobileMenuTrigger(x) {
-    x.classList.toggle("change");
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+
+  console.log(currentScrollPos)
+
+  // 20 is an arbitrary number here, just to make you think if you need the prevScrollpos variable:
+  if (currentScrollPos > 20) {
+    // I am using 'display' instead of 'top':
+    document.getElementById("navbar-sticky").style.display = "flex";
+
+  } else {
+    document.getElementById("navbar-sticky").style.display = "none";
   }
+}
